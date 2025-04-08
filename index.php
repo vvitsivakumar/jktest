e280562f2510d02d57acc8a861be4ca60b656717
Sivakumar <vvitsivakumar@gmail.com>
2025-04-08 14:43:13 +0530

    test-commit

--- index.php
+++ index.php
@@ -1,15 +1,3 @@
 <?php
-// Sample user data
-$users = [
-    ['id' => 1, 'name' => 'Siva', 'email' => 'siva@example.com'],
-    ['id' => 2, 'name' => 'Arun', 'email' => 'arun@example.com'],
-];
-
-// Display users
-echo "<h2>User List:</h2>";
-echo "<ul>";
-foreach ($users as $user) {
-    echo "<li>ID: {$user['id']} | Name: {$user['name']} | Email: {$user['email']}</li>";
-}
-echo "</ul>";
+echo "Hello, Sivakumar! Jenkins PHP Build Test Successful 🚀";
 ?>
