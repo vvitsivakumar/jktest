86b4280ebeb35ad0802800d8b276a834d859d7d8
Sivakumar <vvitsivakumar@gmail.com>
2025-04-08 14:29:04 +0530

    new code

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
-echo "✅ Hello from Jenkins Freestyle!";
+// Sample user data
+$users = [
+    ['id' => 1, 'name' => 'Siva', 'email' => 'siva@example.com'],
+    ['id' => 2, 'name' => 'Arun', 'email' => 'arun@example.com'],
+];
+
+// Display users
+echo "<h2>User List:</h2>";
+echo "<ul>";
+foreach ($users as $user) {
+    echo "<li>ID: {$user['id']} | Name: {$user['name']} | Email: {$user['email']}</li>";
+}
+echo "</ul>";
 ?>
